63d749c99bd69a8ffbc33efaea42352fed09b342
Débora Moyano <debo4606@gmail.com>
2026-03-31 22:26:59 -0300

    vista xml y estructura mvvm

---
+++ Conversor/app/src/main/java/com/debora/conversor/MainActivityViewModel.java
@@ -0,0 +1,15 @@
+package com.debora.conversor;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+}
+
