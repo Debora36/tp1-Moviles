b134ca947ff32cbc70716dd52e20f5984ddee00d
Débora Moyano <debo4606@gmail.com>
2026-04-04 00:03:44 -0300

    metodo convertir
    un sólo método para la conversión y codigo comentado

--- Conversor/app/src/main/java/com/debora/conversor/MainActivityViewModel.java
+++ Conversor/app/src/main/java/com/debora/conversor/MainActivityViewModel.java
@@ -9,8 +9,8 @@ import androidx.lifecycle.MutableLiveData;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
-    private float uneuro=0.86f;
-    private MutableLiveData <Float> UnEuro;
+    private float uneuro=0.86f; //valor de 1 euro por defecto
+    private MutableLiveData <Float> UnEuro;//Mutable que guarda valor actual de conversión y avisa si cambia
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         UnEuro = new MutableLiveData<>();
@@ -20,19 +20,23 @@ public class MainActivityViewModel extends AndroidViewModel {
     public LiveData<Float> getUnEuro(){
         return UnEuro;
     }
+    // Cambiar el valor de conversión
     public void setUnEuro(float nuevoValor) {
         if (UnEuro != null) {
             UnEuro.setValue(nuevoValor);
         }
     }
 
-    public float convertirADolar(float euro){
+    // Convertir un monto a dolar o a euro segun el boolean
+    public float convertir(float monto, boolean aDolar){
         Float valorActual=UnEuro.getValue();
-        return euro/valorActual;
-    }
-    public float convertirAEuro(float dolar){
-        Float valorActual=UnEuro.getValue();
-        return dolar*valorActual;
+        Float resultado;
+        if (aDolar){//Convertir euros a dolares
+            resultado=monto/valorActual;
+        }else{//Convertir Dolar a Euros
+            resultado=monto*valorActual;
+        }
+        return resultado;
     }
 
 
